e645b89a4b16a398082a96009621262d81f44c27
itsDalcaN <dsumindan@oakland.edu>
2021-10-11 19:29:02 -0400

    Clean slate

--- GrizzFit/App.js
+++ GrizzFit/App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { registerUser } from './services/login';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title}>GrizzFit</Text>
+      <Button 
+        onPress={registerUser}
+        title="Tickle Me"
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +23,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    fontSize: 25,
+  }
 });
---
+++ GrizzFit/services/login.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { f, auth } from '../config/config';
+
+export const registerUser = () => {
+    console.log('richard have fun implementing the firebase auth :)');
+}

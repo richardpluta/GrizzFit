2a6d803d07304e00d95ad4456cd18e2a04530c79
itsDalcaN <dsumindan@oakland.edu>
2021-11-02 22:43:53 -0400

    Exercise Repo as home page

--- GrizzFit/App.js
+++ GrizzFit/App.js
@@ -1,17 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { registerUser } from './services/login';
+import { View, StyleSheet } from 'react-native';
+import ExerciseRepo from './screens/ExerciseRepo';
+import { darkModePalette } from './styles/DarkModePalette';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>GrizzFit</Text>
-      <Button 
-        onPress={registerUser}
-        title="Exercise Repo"
-      />
-      <StatusBar style="auto" />
+      <ExerciseRepo />
     </View>
   );
 }
@@ -19,11 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: darkModePalette.shadowAlt,
   },
-  title: {
-    fontSize: 25,
-  }
 });

5cccbd6ba6cf11361727ded125bd7c220c9d882e
itsDalcaN <dsumindan@oakland.edu>
2021-11-16 20:37:46 -0500

    Spiced up the exercise info screen

--- GrizzFit/src/screens/ExerciseInfo.js
+++ GrizzFit/src/screens/ExerciseInfo.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, TouchableOpacity, Image } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
 import { darkModePalette } from '../styles/DarkModePalette';
 
 export default function ExerciseInfo({ route, navigation }) {
     const {item} = route.params;
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>EXERCISE INFO on {item.name}</Text>
-            
+        <ScrollView style={styles.container} contentContainerStyle={{
+            justifyContent: 'center',
+            alignItems: 'center',}}>
+            <View style={styles.imageBorder}>
+                <Image style={styles.formGif} source={{uri: item.formGifUrl}} />
+            </View>
+            <Text style={styles.exerciseName}>{item.name}</Text>
+            <Text style={styles.instructions}>{item.instructions}</Text>
             <View>
-                <TouchableOpacity
+                <TouchableOpacity style={styles.button}
                     onPress={() => navigation.pop()}
                 >
-                    <Text style={styles.text}>Go back</Text>
+                    <Text style={{color: darkModePalette.black}}>Back</Text>
                 </TouchableOpacity>
             </View>
-        </View>
+        </ScrollView>
     );
 }
 
@@ -25,8 +31,31 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: darkModePalette.shadowAlt,
     },
-    text: {
+    exerciseName: {
+        padding: 10,
+        fontSize: 22,
+        color: darkModePalette.white,
+    },
+    instructions: {
+        padding: 10,
         fontSize: 16,
         color: darkModePalette.white,
+    },
+    formGif: {
+        width: 500,
+        height: 250,
+    },
+    imageBorder: {
+        borderBottomWidth: 3,
+        borderBottomColor: "#000",
+    },
+    button: {
+        backgroundColor: darkModePalette.primary,
+        borderRadius: 10,
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        alignItems: 'center',
+        width: '60%',
+        margin: 20,
     }
 });
